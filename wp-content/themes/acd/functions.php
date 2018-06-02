3cf4f5a021a4d970942560a3a30433602976e36c
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-02 15:21:51 +1000

    Added theme settings

--- wp-content/themes/acd/functions.php
+++ wp-content/themes/acd/functions.php
@@ -1,12 +1,76 @@
 <?php
+
+/* ------------------------------------------------------------------------ *
+ * Setting Registration
+ * ------------------------------------------------------------------------ */
+
 /**
- * Created by PhpStorm.
- * User: pixie
- * Date: 2/6/18
- * Time: 12:53 PM
+ * Initializes the theme options page by registering the Sections,
+ * Fields, and Settings.
+ *
+ * This function is registered with the 'admin_init' hook.
  */
 
-// Gutenberg blocks
+add_action( 'admin_menu', 'create_acd_settings_page');
+function create_acd_settings_page() {
+
+	  add_menu_page(
+				'ACD Settings',
+				'ACD',
+				'manage_options',
+				'acd_settings',
+				'acd_settings_page_content',
+				'dashicons-admin-plugins',
+				 100
+	  );
+}
+
+add_action('admin_init', 'acd_initialize_theme_options');
+function acd_initialize_theme_options() {
+
+    // First, we register a section. This is necessary since all future options must belong to one.
+    add_settings_section(
+        'acd_settings_section',         // ID used to identify this section and with which to register options
+        'ACD Options',                  // Title to be displayed on the administration page
+        'acd_general_options_callback', // Callback used to render the description of the section
+        'acd_settings'                           // Page on which to add this section of options
+    );
+
+} // end sandbox_initialize_theme_options
+
+function acd_settings_page_content() {
+	echo '<div class="wrap"><h2>ACD</h2><form method="post" action="options.php">';
+	echo settings_fields( 'acd_settings' );
+	echo do_settings_sections( 'acd_settings' );
+	echo submit_button();
+	echo '</form></div>';
+}
+
+/* ------------------------------------------------------------------------ *
+ * Section Callbacks
+ * ------------------------------------------------------------------------ */
+
+function acd_general_options_callback() {
+    echo '<p>Fill in configurable options for the ACD site</p>';
+} // end sandbox_general_options_callback
+
+/* ------------------------------------------------------------------------ *
+ * Field Callbacks
+ * ------------------------------------------------------------------------ */
+
+
+
+function custom_theme_setup() {
+	add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'custom_theme_setup' );
+
+
+//add_filter('xmlrpc_enabled', '__return_false'); // Possible attack vulnerability
+
+/* ------------------------------------------------------------------------ *
+ * Gutenberg Blocks
+ * ------------------------------------------------------------------------ */
 
 function block_homepage_slider() {
     // Scripts.
@@ -32,7 +96,6 @@ function block_homepage_slider() {
 add_action( 'init', 'block_homepage_slider' );
 
 
-
 /*function gutenberg_boilerplate_block() {
     wp_register_script(
         'gutenberg-boilerplate-es5-step01',
@@ -45,3 +108,4 @@ add_action( 'init', 'block_homepage_slider' );
     ) );
 }
 add_action( 'init', 'gutenberg_boilerplate_block' ); */
+
