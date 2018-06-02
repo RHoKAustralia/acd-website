65469d386b83457b3e0910263b90f9d89265430b
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-02 21:56:34 +1000

    Footer update

--- wp-content/themes/acd/footer.php
+++ wp-content/themes/acd/footer.php
@@ -30,9 +30,9 @@
             <section>
                 <h2>Follow Us</h2>
                 <div class="row">
-                    <div class="4u 12u(mobile)"><a href="#" class="button big icon fa-twitter">8.4k</a></div>
-                    <div class="4u 12u(mobile)"><a href="#" class="button big alt icon fa-facebook">14.2k</a></div>
-                    <div class="4u 12u(mobile)"><a href="#" class="button big alt2 icon fa-instagram">2.8k</a></div>
+                    <div class="6u 12u(mobile)"><a href="https://twitter.com/ACDinfo" target="_blank" class="button big icon fa-twitter">@ACDinfo</a></div>
+                    <div class="6u 12u(mobile)"><a href="https://www.facebook.com/acdvic" target="_blank" class="button big alt icon fa-facebook">ACD (Vic)</a></div>
+                    <!--<div class="4u 12u(mobile)"><a href="#" class="button big alt2 icon fa-instagram">2.8k</a></div> -->
                 </div>
             </section>
             <section>
@@ -53,10 +53,10 @@
                     <div class="6u 12u(mobile)">
                         <ul class="style3">
                             <li class="icon fa-facebook">
-                                <a href="https://www.facebook.com/acdvic" target="_blank" class="">ACDvic Page</a>
+                                <a  class="">ACDvic Page</a>
                             </li>
                             <li class="icon fa-twitter">
-                                <a href="https://twitter.com/ACDinfo" target="_blank">@ACDinfo</a>
+                                <a >@ACDinfo</a>
                             </li>
                         </ul>
                     </div>
