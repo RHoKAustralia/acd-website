ebf90df753e29493bb6509ba52c8223e1a7d9824
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-02 13:01:42 +1000

    Inital commit from Pixelarity Momentum template

---
+++ wp-content/themes/acd/footer.php
@@ -0,0 +1,73 @@
+<footer id="footer" class="container">
+    <div class="row">
+        <div class="3u 12u(mobile)">
+            <section>
+                <h2>Duis adipiscing</h2>
+                <ul class="style1">
+                    <li class="first"><a href="#">Quis lacus praesent et aliquet</a></li>
+                    <li><a href="#">Sagittis nisi vel dolore dignissim</a></li>
+                    <li><a href="#">Pellentesque nulla adipiscing</a></li>
+                    <li><a href="#">Quis lacus praesent et aliquet</a></li>
+                    <li><a href="#">Sagittis nisi vel dolore dignissim</a></li>
+                    <li><a href="#">Pellentesque nulla adipiscing</a></li>
+                    <li><a href="#">Quis lacus praesent et aliquet</a></li>
+                    <li><a href="#">Sagittis nisi vel dolore dignissim</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="3u 12u(mobile)">
+            <section>
+                <h2>Sed bibendum</h2>
+                <ul class="style1">
+                    <li class="first"><a href="#">Sagittis nisi vel dolore dignissim</a></li>
+                    <li><a href="#">Pellentesque nulla adipiscing</a></li>
+                    <li><a href="#">Quis lacus praesent et aliquet</a></li>
+                    <li><a href="#">Sagittis nisi vel dolore dignissim</a></li>
+                    <li><a href="#">Pellentesque nulla adipiscing</a></li>
+                    <li><a href="#">Quis lacus praesent et aliquet</a></li>
+                    <li><a href="#">Sagittis nisi vel dolore dignissim</a></li>
+                    <li><a href="#">Pellentesque nulla adipiscing</a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="6u 12u(mobile)">
+            <section>
+                <h2>Follow Us</h2>
+                <div class="row">
+                    <div class="4u 12u(mobile)"><a href="#" class="button big icon fa-twitter">8.4k</a></div>
+                    <div class="4u 12u(mobile)"><a href="#" class="button big alt icon fa-facebook">14.2k</a></div>
+                    <div class="4u 12u(mobile)"><a href="#" class="button big alt2 icon fa-instagram">2.8k</a></div>
+                </div>
+            </section>
+            <section>
+                <h2>Contact Us</h2>
+                <div class="row">
+                    <div class="6u 12u(mobile)">
+                        <ul class="style3">
+                            <li class="icon fa-envelope">
+                                <a href="#" class="">information@untitled.tld</a>
+                            </li>
+                            <li class="icon fa-home">
+                                1234 Somewhere Road<br />
+                                Nashville, TN 00000-0000
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="6u 12u(mobile)">
+                        <ul class="style3">
+                            <li class="icon fa-twitter">
+                                <a href="#">@untitled-corp</a>
+                            </li>
+                            <li class="icon fa-phone">
+                                (800) 555-0000
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <div id="copyright">
+        &copy; Untitled. All rights reserved.
+    </div>
+</footer>
---
+++ wp-content/themes/acd/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pixie
+ * Date: 2/6/18
+ * Time: 12:53 PM
+ */
