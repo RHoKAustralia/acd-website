da229c8a59fe4854a801827fc8a34187baa9c02f
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-07 20:22:17 +1000

    template parts to default page

---
+++ wp-content/themes/acd/template-parts/content.php
@@ -0,0 +1,7 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+       <?php
+            the_title( '<h2 class="entry-title">', '</h2>' );
+            the_content(); ?>
+    
+</article><!-- #post-<?php the_ID(); ?> -->
