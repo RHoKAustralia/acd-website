61f1e928b52ac4bf9d4ec1cbb1c1c082ed37c4ce
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-07 19:50:50 +1000

    added gutenberg theme support to functions

--- wp-content/themes/acd/index.php
+++ wp-content/themes/acd/index.php
@@ -117,36 +117,25 @@ License: pixelarity.com/license
 
     </div> -->
 
-    <!-- Featured Wrapper -->
-    <div id="featured-wrapper">
+    <!-- Main Wrapper -->
+    <div id="main-wrapper">
 
-        <!-- Featured -->
-        <div id="featured" class="container">
+        <!-- Main -->
+        <div id="main" class="container">
+            <div class="row">
+                <div class="12u">
+
             <?php if ( have_posts() ) : ?>
             <?php while ( have_posts() ) : the_post(); ?>
-            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="major">
-                    <span class="category">&nbsp;</span>
-                    <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                    <ul class="meta"></ul>
-                </header>
-                <!--<div class="author">< ?php the_author(); ?></div> -->
-                <!--</div><!--end post header-->
-                <div class="entry clear">
+                    <article id="content-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+
                     <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
                     <?php the_content(); ?>
-                    <!--< ?php edit_post_link(); ?> -->
-                    <!-- < ?php wp_link_pages(); ?> </div>
-                <!--end entry-->
-                    <div class="post-footer">
-                        <!-- <div class="comments">< ?php comments_popup_link( 'Leave a Comment', '1 Comment', '% Comments' ); ?></div>
-                    </div><!--end post footer-->
-                    </div><!--end post-->
-                    <?php endwhile; /* rewind or continue if all posts have been fetched */ ?>
-                    <!-- <div class="navigation index">
-                        <div class="alignleft">< ?php next_posts_link( 'Older Entries' ); ?></div>
-                        <div class="alignright">< ?php previous_posts_link( 'Newer Entries' ); ?></div>
-                    </div><!--end navigation-->
+
+                    </article>
+                    <?php endwhile;
                     <?php else : ?>
                     <?php endif; ?>
                 </div>
