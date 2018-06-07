03ac3c0a4ec293f752de52c505050a0515fe5038
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-07 19:55:16 +1000

    added gutenberg theme support to functions

--- wp-content/themes/acd/index.php
+++ wp-content/themes/acd/index.php
@@ -126,18 +126,18 @@ License: pixelarity.com/license
                 <div class="12u">
 
             <?php if ( have_posts() ) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
                     <article id="content-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
+                        <?php the_title(); ?>
 
-                    <?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?>
+                    <!--< ?php if ( function_exists( 'add_theme_support' ) ) the_post_thumbnail(); ?> -->
                     <?php the_content(); ?>
 
                     </article>
-                    <?php endwhile;
-                    <?php else : ?>
-                    <?php endif; ?>
+                <?php endwhile; ?>
+            <?php else : ?>
+            <?php endif; ?>
                 </div>
         </div>
         </div>
