44a44623fda9b698f5f6e65f3f27b027172a97a7
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-19 22:24:33 +1000

    Social icons test

--- wp-content/themes/acd/header.php
+++ wp-content/themes/acd/header.php
@@ -14,7 +14,7 @@
                     <div class="6u 12u$(mobile)"><a class="button icon fa-map-marker headerCorners" style="cursor: pointer;\width:100%;text-align:center;" href="#contactus">Contact Us</a></div>
                     <div class="6u 12u$(mobile)"><a class="button icon alt fa-usd headerCorners" style="cursor: pointer;width:100%;text-align:center;" href="#">Donate</a></div>
                     <div class="8u 12u$(mobile)">
-                        <input type="search" name="search" id="search" placeholder="Search" style="height: 1.5em;">
+                        <input type="text" name="search" id="search" placeholder="Search" style="min-height:40px;min-width:98%;valign:middle;line-height:2em">
                     </div>
                     <div class="1u 12u$(mobile)">
                         <div class="social"><a class="fa fa-facebook" title="" target="_blank" href="#" style="cursor: pointer;"></a></div>
