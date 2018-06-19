1ae6521a8ec846202c6dfaf3ee3fc348c0e110e9
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-19 21:25:56 +1000

    Header

--- wp-content/themes/acd/header.php
+++ wp-content/themes/acd/header.php
@@ -13,12 +13,12 @@
                 <div class="row uniform">
                     <ul class="actions fit 6u">
                         <li>
-                            <a class="button primary icon fa-map-marker headerCorners" style="cursor: pointer;" href="#contactus">Contact Us</a>
+                            <a class="button big icon fa-map-marker headerCorners" style="cursor: pointer;" href="#contactus">Contact Us</a>
                         </li>
                     </ul>
                     <ul class="actions fit 6u">
                        <li>
-                           <a class="button primary icon fa-donate headerCorners" style="cursor: pointer;" href="#">Donate</a>
+                           <a class="button big icon fa-usd headerCorners" style="cursor: pointer;" href="#">Donate</a>
                        </li>
                     </ul>
                 </div>
