7b7e202d29268e3d98be3df476bac249a5aae562
Samara N <3608343+EvilAngelPixie@users.noreply.github.com>
2018-06-19 20:40:08 +1000

    Event section tweaks

--- wp-content/themes/acd/header.php
+++ wp-content/themes/acd/header.php
@@ -6,10 +6,10 @@
 
         <!-- Header -->
         <div class="row">
-            <div class="6u">
+            <div class="6u 12u$(mobile)">
                 <span class="image fit"><img src="<?php echo get_option('acd_logo'); ?>" href="index.php"></span>
             </div>
-            <div class="6u">
+            <div class="6u 12u$(mobile)">
                <ul class="actions">
                    <li>
                        <a class="button primary icon fa-map-marker" style="cursor: pointer;" href="#contactus">Contact Us</a>
@@ -17,13 +17,13 @@
                    </li>
                </ul>
             </div>
-            <div class="12u">
+            <div class="12u 12u$(mobile)">
                 <form method="post" action="#" class="combined">
                     <input type="search" name="search" id="search" placeholder="Search" class="invert">
                     <input type="submit" class="special" value="Search">
                 </form>
             </div>
-            <div class="12u">
+            <div class="12u 12u$(mobile)">
                 <ul class="actions">
                     <li>
                         <a class="button primary icon fa-map-marker" style="cursor: pointer;" href="#contactus">Contact Us</a>
